549bf8064e5e1cb499a05a399de493fac4f7d270
Matthew Nieva <matthewcn56@g.ucla.edu>
2022-05-30 13:49:52 -0700

    added lykdat classification

--- screens/Authenticated/Profile.tsx
+++ screens/Authenticated/Profile.tsx
@@ -12,10 +12,21 @@ import { TouchableOpacity } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import {deleteUploadedImage} from "../../db/firebaseFunctions";
 import * as ImagePicker from 'expo-image-picker';
-import { grabUserImages } from "../../db/mongoFunctions";
+import { grabUserImages, findAssociatedProducts } from "../../db/mongoFunctions";
+import { ActivityIndicator, Colors, Modal, Portal, Provider } from 'react-native-paper';
+
+
+function findImage(pics: UploadedPicture[], imageName: string){
+    for (const pic of pics){
+        if (pic.image_name === imageName)
+            return pic;
+    }
+    return false;
+}
 
 type ProfileProps = MaterialBottomTabScreenProps<ProfileStackParamList, 'ProfilePage'>;
 export default function Profile(props: ProfileProps) {
+    const [isLoading, setIsLoading] = useState(false);
     const {logout, userPics, setUserPics } = useContext(AuthContext);
     function moveToOutfitPage(pic: UploadedPicture){
         props.navigation.navigate('Outfit', {
@@ -39,19 +50,20 @@ export default function Profile(props: ProfileProps) {
         )
     }
     
-    const PicIcons = userPics.map(pic=> (<PicItem pic={pic} key={pic.image_name} moveToOutfitPage={moveToOutfitPage} />));
+    const PicIcons = userPics.map(pic=> (<PicItem pic={pic} key={pic.image_name} moveToOutfitPage={moveToOutfitPage} setIsLoading={setIsLoading} />));
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.profileSquare1}></View>
-            <View style={styles.profileSquare2}></View>
-            
-            <View style={styles.profileContainer3}>
+                <View style={styles.profileSquare1}></View>
+                <View style={styles.profileSquare2}></View>
                 
-                <Text style={styles.profileTitleText}>Profile</Text>
-                <Button color={PRIMARY_COLOR} labelStyle={{ color: '#fff', fontSize: 12 }} contentStyle={styles.buttonBackInner} style={styles.profileLogoutButton}onPress={() => attemptLogout()} mode="contained" >Log Out</Button>
-                <Text style={styles.profileSubtitleText}>Your Uploaded Pics</Text>
-                <ScrollView style={styles.profileContainer2}>{PicIcons}</ScrollView>
-            </View>
+                <View style={styles.profileContainer3}>
+                    <Text style={styles.profileTitleText}>Profile</Text>
+                    <Button color={PRIMARY_COLOR} labelStyle={{ color: '#fff', fontSize: 12 }} contentStyle={styles.buttonBackInner} style={styles.profileLogoutButton}onPress={() => attemptLogout()} mode="contained" >Log Out</Button>
+                    <Text style={styles.profileSubtitleText}>Your Uploaded Pics</Text>
+                    { isLoading ? <LoadingModal/> :
+                    <ScrollView style={styles.profileContainer2}>{PicIcons}</ScrollView>
+    }
+                </View>
         </SafeAreaView>
     )
 }
@@ -59,6 +71,7 @@ export default function Profile(props: ProfileProps) {
 interface PicItemProps {
     pic: UploadedPicture,
     moveToOutfitPage: (pic: UploadedPicture) => void
+    setIsLoading: (val : boolean) => void
 }
 function PicItem(props: PicItemProps){
     const {user, userToken, setUserPics} = useContext(AuthContext);
@@ -110,11 +123,30 @@ function PicItem(props: PicItemProps){
             <View >
                 <TouchableOpacity 
                 activeOpacity={0.9}
-                onPress={() => {
+                onPress={async () => {
                     if(props.pic.categoryNames && props.pic.similarClothes)
                         props.moveToOutfitPage(props.pic);
                     else {
-                        alert("No related images found yet!");
+                        //alert("Please wait, getting related clothes! May take 30 seconds. Sit tight!");
+                        props.setIsLoading(true);
+                        const associatedRes = await findAssociatedProducts(props.pic.image_name, userToken);
+                        if(associatedRes){
+                            const newImages= await grabUserImages(userToken);
+                            const newImage = findImage(newImages,props.pic.image_name);
+                            setUserPics(newImages);
+                            if(newImage){
+                                props.setIsLoading(false);
+                                props.moveToOutfitPage(newImage);
+                            }
+                            else {
+                                alert("Error moving!");
+                                props.setIsLoading(false);
+                            }
+                        }
+                        else {
+                            props.setIsLoading(false);
+                        }
+                        
                     }
                 }}
                     onLongPress={() => attemptDelete(props.pic)}>
@@ -131,6 +163,15 @@ function PicItem(props: PicItemProps){
                 }
             </View>
             <Text style={styles.profileText}>{props.pic.image_name}</Text>
+        </View>
+    )
+}
+
+function LoadingModal(){
+    return(
+        <View>
+            <ActivityIndicator animating={true} color={Colors.red800} />
+            <Text>Sit tight, our tailors are at work finding the best clothes for you!</Text> 
         </View>
     )
 }
